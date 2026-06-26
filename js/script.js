094f48ecae512335fdda1577f8b8bf4659d8eb80
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2026-06-25 23:30:12 -0300

    subi pro git

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//window.location.href
+window.alert("Bem vindo!")
+window.confirm("Você têm certeza que deseja entrar nesse site ?")
+function mudar_text(){
+window.document.getElementById("titulo_form").innerText = "alterei o titulo"
+}
+function mudar_cor(){
+window.document.getElementById("titulo_form").style.color = "red";
+}
+function mudar_cor2(){
+window.document.getElementById("titulo_form").style.color = "black"
+}
+
